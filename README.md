c2b290b7fc2da930eaf68c73311d3dcceeb7e627
TassioSales <74218122+TassioSales@users.noreply.github.com>
2021-08-30 14:08:33 -0300

    commit inicial

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
-# DetectorPlagio_COH_PIAH
+<h1>Detector de Plagio</h1>
+<br>
+
+<h1>Tarefa de programação: Programa completo - Similaridades entre textos - Caso COH-PIAH</h1>
+
+<h2>Prólogo</h2>
+
+<p>Neste último exercício da Parte 1, iremos praticar não só o que vimos até agora no curso mas também outra habilidade importante de um programador: utilizar e interagir com código escrito por terceiros. Aqui, você não irá implementar o seu programa do zero. Você irá partir de um programa já iniciado e irá completá-lo. Na verdade, esse é o caso mais comum na indústria de software, onde muitos desenvolvedores trabalham colaborativamente em um mesmo programa.</p>
+
+
+
+<h2>Introdução </h2>
+
+<p>Manuel Estandarte é monitor na disciplina Introdução à Produção Textual I na Universidade de Pasárgada (UPA). Durante o período letivo, Manuel descobriu que uma epidemia de COH-PIAH estava se espalhando pela UPA. Essa doença rara e altamente contagiosa faz com que indivíduos contaminados produzam, involuntariamente, textos muito semelhantes aos de outras pessoas. Após a entrega da primeira redação, Manuel desconfiou que alguns alunos estavam sofrendo de COH-PIAH. Manuel, preocupado com a saúde da turma, resolveu buscar um método para identificar os casos de COH-PIAH. Para isso, ele necessita da sua ajuda para desenvolver um programa que o auxilie a identificar os alunos contaminados.</p>
+
+<h2>Detecção de autoria</h2>
+
+<p>Diferentes pessoas possuem diferentes estilos de escrita; por exemplo, algumas pessoas preferem sentenças mais curtas, outras preferem sentenças mais longas. Utilizando diversas estatísticas do texto, é possível identificar aspectos que funcionam como uma “assinatura” do seu autor e, portanto, é possível detectar se dois textos dados foram escritos por uma mesma pessoa. Ou seja, essa “assinatura” pode ser utilizada para detecção de plágio, evidência forense ou, neste caso, para diagnosticar a grave doença COH-PIAH.</p>
+
+<h2>Concluindo</h2>
+
+<p>
+    Basicamente, sua tarefa é implementar corretamente as seguintes funções:
+    compara_assinatura(as_a, as_b)
+    calcula_assinatura(texto)
+    avalia_textos(textos, ass_cp)
+    Usando o esqueleto que oferecemos acima e implementando essas 3 funções, seu detector de plágio estará completo e você poderá submetê-lo ao corretor automático. Caso o corretor automático aponte erros, tente ler com bastante cuidado e atenção a mensagem fornecida por ele, pois ela normalmente ajuda a identificar o erro.
+    
+    Boa sorte! Não desista!
+    
+    Sabemos que é um desafio, mas você vai aprender muito com ele.
+    
+    Pense no prazer que você vai sentir quando sua solução final for aceita!!!
+</p>
---
+++ coh_piah.py
@@ -0,0 +1,141 @@
+import re
+
+def le_assinatura():
+    '''A funcao le os valores dos tracos linguisticos do modelo e devolve uma assinatura a ser comparada com os textos fornecidos'''
+    print("Bem-vindo ao detector automático de COH-PIAH.")
+    print("Informe a assinatura típica de um aluno infectado:")
+
+    wal = float(input("Entre o tamanho médio de palavra:"))
+    ttr = float(input("Entre a relação Type-Token:"))
+    hlr = float(input("Entre a Razão Hapax Legomana:"))
+    sal = float(input("Entre o tamanho médio de sentença:"))
+    sac = float(input("Entre a complexidade média da sentença:"))
+    pal = float(input("Entre o tamanho medio de frase:"))
+
+    return [wal, ttr, hlr, sal, sac, pal]
+
+def le_textos():
+    '''A funcao le todos os textos a serem comparados e devolve uma lista contendo cada texto como um elemento'''
+    i = 1
+    textos = []
+    texto = input("Digite o texto " + str(i) +" (aperte enter para sair):")
+    while texto:
+        textos.append(texto)
+        i += 1
+        texto = input("Digite o texto " + str(i) +" (aperte enter para sair):")
+
+    return textos
+
+def separa_sentencas(texto):
+    '''A funcao recebe um texto e devolve uma lista das sentencas dentro do texto'''
+    sentencas = re.split(r'[.!?]+', texto)
+    if sentencas[-1] == '':
+        del sentencas[-1]
+    return sentencas
+
+def separa_frases(sentenca):
+    '''A funcao recebe uma sentenca e devolve uma lista das frases dentro da sentenca'''
+    return re.split(r'[,:;]+', sentenca)
+
+def separa_palavras(frase):
+    '''A funcao recebe uma frase e devolve uma lista das palavras dentro da frase'''
+    return frase.split()
+
+def n_palavras_unicas(lista_palavras):
+    '''Essa funcao recebe uma lista de palavras e devolve o numero de palavras que aparecem uma unica vez'''
+    freq = dict()
+    unicas = 0
+    for palavra in lista_palavras:
+        p = palavra.lower()
+        if p in freq:
+            if freq[p] == 1:
+                unicas -= 1
+            freq[p] += 1
+        else:
+            freq[p] = 1
+            unicas += 1
+
+    return unicas
+
+def n_palavras_diferentes(lista_palavras):
+    '''Essa funcao recebe uma lista de palavras e devolve o numero de palavras diferentes utilizadas'''
+    freq = dict()
+    for palavra in lista_palavras:
+        p = palavra.lower()
+        if p in freq:
+            freq[p] += 1
+        else:
+            freq[p] = 1
+
+    return len(freq)
+
+def compara_assinatura(as_a, as_b):
+    '''IMPLEMENTAR. Essa funcao recebe duas assinaturas de texto e deve devolver o grau de similaridade nas assinaturas.'''
+
+    resultado_diferenca = []
+    
+    for i in range (0, len(assinatura_calculada), 1):
+        resultado_diferenca.append(abs(assinatura_infectada[i] - assinatura_calculada[i]))
+
+    soma_diferenca = 0
+    for resultado in resultado_diferenca:
+        soma_diferenca += resultado
+
+    grau_similaridade = soma_diferenca / 6
+
+    return grau_similaridade
+
+def calcula_assinatura(texto):
+    '''IMPLEMENTAR. Essa funcao recebe um texto e deve devolver a assinatura do texto.'''
+
+    texto = texto.lower()
+    total_letras = 0
+    lista_palavras = []
+    lista_frases = []
+    
+    lista_sentencas = separa_sentencas(texto)
+    for sentenca in lista_sentencas:
+      novas_frases = separa_frases(sentenca)
+      lista_frases.extend(novas_frases)
+      
+    for frase in lista_frases:
+      novas_palavras = separa_palavras(frase)
+      lista_palavras.extend(novas_palavras)
+      
+    for palavra in lista_palavras:
+      total_letras += len(palavra)
+    
+    wal_texto = total_letras / len(lista_palavras)
+    
+    # ttr_texto = Relação Type-Token
+    ttr_texto = n_palavras_diferentes(lista_palavras) / len(lista_palavras)
+
+    # hlr_texto = Razão Hapax Legomana
+    hlr_texto = n_palavras_unicas(lista_palavras) / len(lista_palavras)
+    
+    # sal_texto = Tamanho médio de sentença
+    caracteres_sentencas = 0
+    for sentenca in lista_sentencas:
+      caracteres_sentencas += len(sentenca)
+    sal_texto = caracteres_sentencas / len(lista_sentencas)
+    
+    # sac_texto = Complexidade média da sentença
+    sac_texto =  len(lista_frases) / len(lista_sentencas)
+    
+    # pal_texto = Tamanho médio de frase
+    caracteres_frases = 0
+    for frase in lista_frases:
+      caracteres_frases += len(frase)
+    pal_texto = caracteres_frases / len(lista_frases)
+   
+    return [wal_texto, ttr_texto, hlr_texto, sal_texto, sac_texto, pal_texto]
+
+def avalia_textos(textos, ass_cp):
+    '''IMPLEMENTAR. Essa funcao recebe uma lista de textos e uma assinatura ass_cp e deve devolver o numero (1 a n) do texto com maior probabilidade de ter sido infectado por COH-PIAH.'''
+    assinatura_texto = []
+    lista_assinaturas = []
+    for texto in textos:
+        assinatura_texto = calcula_assinatura(texto)
+        lista_assinaturas.append(compara_assinatura(assinatura_texto, assinatura_infectada))
+    
+    return lista_assinaturas.index(max(lista_assinaturas))
