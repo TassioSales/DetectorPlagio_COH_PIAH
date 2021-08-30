b08902991d6d409b763d4c88056f37603ff1dc3c
TassioSales <74218122+TassioSales@users.noreply.github.com>
2021-08-30 14:52:12 -0300

    Update coh_piah.py

--- coh_piah.py
+++ coh_piah.py
@@ -1,11 +1,28 @@
 import re
+import math
 
+# Retorna a quantidade de caracteres de um conjunto de palavras,
+# frases ou sentença
+def quantidadeTotalDeCaracteres(conjuntos):
+    quantidadeCaracteres = 0
+
+    for conjunto in conjuntos:
+        quantidadeCaracteres += len(conjunto)
+
+    return quantidadeCaracteres
+
+# Retorna o tamanho médio das palavras em uma lista de palavras
+def TamanhoMedioDasPalavras(palavras):
+
+    return (quantidadeTotalDeCaracteres(palavras) / len(palavras))
+
+# A funcao le os valores dos tracos linguisticos do modelo e devolve uma
+# assinatura a ser comparada com os textos fornecidos
+# padrão: [4.79, 0.72, 0.56, 80.5, 2.5, 31.6]
 def le_assinatura():
-    '''A funcao le os valores dos tracos linguisticos do modelo e devolve uma assinatura a ser comparada com os textos fornecidos'''
     print("Bem-vindo ao detector automático de COH-PIAH.")
-    print("Informe a assinatura típica de um aluno infectado:")
 
-    wal = float(input("Entre o tamanho médio de palavra:"))
+    wal = float(input("Entre o tamanho medio de palavra:"))
     ttr = float(input("Entre a relação Type-Token:"))
     hlr = float(input("Entre a Razão Hapax Legomana:"))
     sal = float(input("Entre o tamanho médio de sentença:"))
@@ -15,7 +32,6 @@ def le_assinatura():
     return [wal, ttr, hlr, sal, sac, pal]
 
 def le_textos():
-    '''A funcao le todos os textos a serem comparados e devolve uma lista contendo cada texto como um elemento'''
     i = 1
     textos = []
     texto = input("Digite o texto " + str(i) +" (aperte enter para sair):")
@@ -26,23 +42,24 @@ def le_textos():
 
     return textos
 
+# A funcao recebe um texto e devolve uma lista das sentencas dentro do texto
 def separa_sentencas(texto):
-    '''A funcao recebe um texto e devolve uma lista das sentencas dentro do texto'''
     sentencas = re.split(r'[.!?]+', texto)
     if sentencas[-1] == '':
         del sentencas[-1]
     return sentencas
 
+# A funcao recebe uma sentenca e devolve uma lista das frases dentro da sentenca
 def separa_frases(sentenca):
-    '''A funcao recebe uma sentenca e devolve uma lista das frases dentro da sentenca'''
     return re.split(r'[,:;]+', sentenca)
 
+# A funcao recebe uma frase e devolve uma lista das palavras dentro da frase
 def separa_palavras(frase):
-    '''A funcao recebe uma frase e devolve uma lista das palavras dentro da frase'''
     return frase.split()
 
+# Essa funcao recebe uma lista de palavras e devolve o numero de palavras
+# que aparecem uma unica vez
 def n_palavras_unicas(lista_palavras):
-    '''Essa funcao recebe uma lista de palavras e devolve o numero de palavras que aparecem uma unica vez'''
     freq = dict()
     unicas = 0
     for palavra in lista_palavras:
@@ -57,8 +74,9 @@ def n_palavras_unicas(lista_palavras):
 
     return unicas
 
+# Essa funcao recebe uma lista de palavras e devolve o numero de palavras
+# diferentes utilizadas
 def n_palavras_diferentes(lista_palavras):
-    '''Essa funcao recebe uma lista de palavras e devolve o numero de palavras diferentes utilizadas'''
     freq = dict()
     for palavra in lista_palavras:
         p = palavra.lower()
@@ -68,78 +86,56 @@ def n_palavras_diferentes(lista_palavras):
             freq[p] = 1
 
     return len(freq)
-    
+
+# IMPLEMENTAR. Essa funcao recebe duas assinaturas de texto e deve devolver o
+# grau de similaridade nas assinaturas.
+def compara_assinatura(as_a, as_b):
+    somatorio = 0
+
+    for i in range(0, 6):
+        somatorio += math.fabs(as_a[i] - as_b[i])
+
+    somatorio = (somatorio / 6)
+    return round(somatorio, 2)
+
+# IMPLEMENTAR. Essa funcao recebe um texto e deve devolver a assinatura do texto.
 def calcula_assinatura(texto):
-    '''IMPLEMENTAR. Essa funcao recebe um texto e deve devolver a assinatura do texto.'''
+    frases = []
+    palavras = []
+
+    # separa as sentencas do texto
     sentencas = separa_sentencas(texto)
 
-    lista_frases = []
-    for s in sentencas:
-        frases_separadas = separa_frases(s)
-        # separa_frases retorna um item de lista para cada sentença
-        # e esses itens são unidos em uma única lista, frases
-        for frases in frases_separadas:
-            lista_frases.append(frases)
-
-    lista_palavras = []
-    for f in lista_frases:
-        palavras_separadas = separa_palavras(f)
-        for palavras in palavras_separadas:
-            lista_palavras.append(palavras)
-    assinatura = []
-
-    assinatura.append(tamanho_medio_palavras(lista_palavras))
-    assinatura.append(relacao_type_token(lista_palavras))
-    assinatura.append(razao_hapax_legomana(lista_palavras))
-    assinatura.append(tamanho_medio_de_sentenca(sentencas))
-    assinatura.append(complexidade_de_sentenca(lista_frases, sentencas))
-    assinatura.append(tamanho_medio_de_frase(lista_frases))
-
-    return assinatura
-
-def avalia_textos(textos, ass_cp):
-    '''IMPLEMENTAR. Essa funcao recebe uma lista de textos e deve devolver o numero (1 a n)
-    do texto com maior probabilidade de ter sido infectado por COH-PIAH.'''
-    valor = ass_cp[0]
-    for x in range(len(ass_cp)):
-        if ass_cp[x] < valor:
-            valor = ass_cp[x]
-            indice = x
-    return indice
-
-# Tamanho médio de palavra é a soma dos tamanhos das palavras dividida pelo número total de palavras:
-def tamanho_medio_palavras(palavras):
-    tamanho_das_palavras = 0
-    total_de_palavras = len(palavras)
-    for palavra in palavras:
-        tamanho_das_palavras = tamanho_das_palavras + len(palavra)
-
-    return tamanho_das_palavras/total_de_palavras
-
-# Relação Type-Token é o número de palavras diferentes dividido pelo número total de palavras.
-def relacao_type_token(palavras):
-    return n_palavras_diferentes(palavras) / len(palavras)
-
-# Razão Hapax Legomana é o número de palavras que aparecem uma única vez dividido pelo total de palavras.
-def razao_hapax_legomana(palavras):
-    return n_palavras_unicas(palavras) / len(palavras)
-
-# Tamanho médio de sentença é a soma dos números de caracteres em todas as sentenças dividida pelo número
-# de sentenças (os caracteres que separam uma sentença da outra não devem ser contabilizados como parte da sentença).
-def tamanho_medio_de_sentenca(sentencas):
-    caracteres_sentenca = 0
+    # separa as frases do texto
     for sentenca in sentencas:
-        caracteres_sentenca = caracteres_sentenca + len(sentenca)
-    return caracteres_sentenca / len(sentencas)
+        frases += separa_frases(sentenca)
 
-# Complexidade de sentença é o número total de frases divido pelo número de sentenças.
-def complexidade_de_sentenca(lista_frases, sentencas):
-    return len(lista_frases) / len(sentencas)
+    #separa as palavras do texto
+    for frase in frases:
+        palavras += separa_palavras(frase)
 
-# Tamanho médio de frase é a soma do número de caracteres em cada frase dividida pelo
-# número de frases no texto (os caracteres que separam uma frase da outra não devem ser contabilizados como parte da frase).
-def tamanho_medio_de_frase(lista_frases):
-    caracteres_frase = 0
-    for frases in lista_frases:
-        caracteres_frase = caracteres_frase + len(frases)
-    return caracteres_frase / len(lista_frases)
+    # define os traços linguisticos
+    wal = TamanhoMedioDasPalavras(palavras)
+    ttr = (n_palavras_diferentes(palavras) / len(palavras))
+    hlr = (n_palavras_unicas(palavras) / len(palavras))
+    sal = (quantidadeTotalDeCaracteres(sentencas) / len(sentencas))
+    sac = (len(frases) / len(sentencas))
+    pal = (quantidadeTotalDeCaracteres(frases) / len(frases))
+
+    return [wal, ttr, hlr, sal, sac, pal]
+
+# IMPLEMENTAR. Essa funcao recebe uma lista de textos e deve devolver o numero
+# (1 a n) do texto com maior probabilidade de ter sido infectado por COH-PIAH.
+def avalia_textos(textos, ass_cp):
+    comparacoes = []
+
+    for texto in textos:
+        assinatura = calcula_assinatura(texto)
+        comparacao = compara_assinatura(assinatura, ass_cp)
+        comparacoes.append(comparacao)
+
+    menorComparacao = min(comparacoes)
+    indiceMenorComparacao = (comparacoes.index(menorComparacao) + 1)
+    print("O autor do texto {} está infectado com COH-PIAH".format(indiceMenorComparacao))
+
+    return indiceMenorComparacao
